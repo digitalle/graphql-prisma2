b1f7c1c92dcf87978cd7ce032a84d2802bb8dfda
Rupert Hurley <rupert.hurley.ext@theculturetrip.com>
2019-01-12 11:26:06 +0000

    init commit for all tracked files

---
+++ prisma-basics/src/index.js
@@ -0,0 +1,28 @@
+import { GraphQLServer } from 'graphql-yoga'
+
+// type defs
+
+const typeDefs = `
+    type Query {
+        hello: String!
+    }
+`;
+
+// resolvers
+const resolvers = {
+    Query: {
+        hello() {
+            return 'this is a query'
+        }
+    }
+}
+
+const server = new GraphQLServer({
+    typeDefs: typeDefs,
+    resolvers: resolvers
+})
+
+
+server.start( () => {
+    console.log("is up");
+})
