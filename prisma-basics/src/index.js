1e3bdaaba006fa1268b34ebfb1d623695e4c1c7f
Rupert Hurley <rupert@digitalle.com>
2019-01-12 16:21:27 +0000

    update

--- prisma-basics/src/index.js
+++ prisma-basics/src/index.js
@@ -1,18 +1,54 @@
 import { GraphQLServer } from 'graphql-yoga'
 
+// 5 Scalar types - String. Boolean, Int, Float, ID = discreet values i.e. NOT an array or Object
+
 // type defs
+
+// title, price, release year, rating, in stock
+
+// location = string
+
+
+// Bio = string sentente
 
 const typeDefs = `
     type Query {
-        hello: String!
+        me: User!
+        post: Post!
+    }
+    
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+        age: Int
+    }
+    
+    type Post {
+        id: ID!
+        title: String!
+        body: String!
+        published: Boolean!        
     }
 `;
 
 // resolvers
 const resolvers = {
     Query: {
-        hello() {
-            return 'this is a query'
+        me() {
+            return {
+                id: '123000',
+                name: 'mike',
+                email: 'mik@op.com'
+            }
+        },
+        post() {
+            return {
+                id: '12345',
+                title: 'go foi it',
+                body: 'here is the body',
+                published: true
+            }
         }
     }
 };
